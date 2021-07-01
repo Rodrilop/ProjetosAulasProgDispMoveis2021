3fd08ff5dc02a10480afde5fb687f95b4585a056
Rodrigo Carlos <64019127+Rodrilop@users.noreply.github.com>
2021-07-01 18:57:59 -0300

    Lista Pets
    Lista Pets

---
+++ ListaPets/app/src/main/java/com/rodrigo/listapets/AnimalEnum.kt
@@ -0,0 +1,7 @@
+package com.rodrigo.listapets
+
+enum class AnimalEnum(val tipo: String) {
+    CACHORRO("Cachorro"),
+    GATO("Gato"),
+    PEIXE("Peixe")
+}
---
+++ ListaPets/app/src/main/java/com/rodrigo/listapets/ColorEnum.kt
@@ -0,0 +1,8 @@
+package com.rodrigo.listapets
+
+enum class ColorEnum(val nome: String) {
+    BRANCO("Branco"),
+    PRETO("Preto"),
+    MARROM("Marrom"),
+    COLORIDO("Colorido")
+}
