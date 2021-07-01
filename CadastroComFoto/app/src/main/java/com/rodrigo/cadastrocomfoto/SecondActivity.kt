7423861708adbe38418dd02c0144fe34f95871cb
Rodrigo Carlos <64019127+Rodrilop@users.noreply.github.com>
2021-07-01 01:29:47 -0300

    Cadastro com Foto
    Cadastro com Foto

---
+++ CadastroComFoto/app/src/main/java/com/rodrigo/cadastrocomfoto/SecondActivity.kt
@@ -0,0 +1,29 @@
+package com.rodrigo.cadastrocomfoto
+
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+    lateinit var textgrid: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        textgrid = findViewById(R.id.txtGrid)
+
+        val usuario = intent.extras?.get("USUARIO") as Usuario
+
+        listaUsuario.add(usuario)
+
+        textgrid.text = listaUsuario.toString()
+
+        Toast.makeText(this, usuario.nome, Toast.LENGTH_LONG).show()
+    }
+
+    companion object{
+        val listaUsuario = mutableListOf<Usuario>()
+    }
+}
